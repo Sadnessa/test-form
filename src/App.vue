fad53f09457050cb7593a6746ea032fe6a434a48
Darya Volkova <anrich@bk.ru>
2022-03-31 09:58:07 +0300

    Added scrollbar.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <FormCard>
-    fdfd
-    <About></About>
-    <Address></Address>
-    <Passport></Passport>
+    <div class="scroll">
+      <About></About>
+      <Address></Address>
+      <Passport></Passport>
+      <!-- <About></About> -->
+    </div>
   </FormCard>
 </template>
 
 <script>
-import FormCard from "./components/base/FormCard.vue"
-import About from "./components/About.vue"
-import Address from "./components/Address.vue"
-import Passport from "./components/Passport.vue"
+import FormCard from "./components/base/FormCard.vue";
+import About from "./components/About.vue";
+import Address from "./components/Address.vue";
+import Passport from "./components/Passport.vue";
 
 export default {
   components: {
@@ -26,6 +28,7 @@ export default {
 <style>
 body {
   background: rgb(212, 212, 212);
+  margin: 0px;
 }
 
 #app {
@@ -34,9 +37,22 @@ body {
   align-items: center;
   height: 100vh;
   margin: 0px 60px;
+  overflow: hidden;
 }
 </style>
 
 <style lang="scss" scoped>
+.card {
+  width: 100%;
+  max-width: 400px;
+  height: 100%;
+  max-height: 600px;
+  overflow: hidden;
 
+  .scroll {
+    height: 100%;
+    overflow-y: auto;
+    // scrollbar-gutter: stable;
+  }
+}
 </style>
