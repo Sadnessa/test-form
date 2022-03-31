faaaa1a3ba5768c6fa19d8ac02e6892d5d709649
Darya Volkova <anrich@bk.ru>
2022-03-31 09:23:49 +0300

    Changed input component.

--- src/components/base/FormInput.vue
+++ src/components/base/FormInput.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="input">
-    <input>
+    <input />
     <slot name="icon"></slot>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.input {
+  display: flex;
+  align-items: center;
+  padding: 4px;
+  background: rgb(212, 212, 212);
+  border-radius: 4px;
+  width: 100%;
+  box-sizing: border-box;
+
+  input {
+    width: 100%;
+    border: none;
+    background: transparent;
+    padding: 4px;
+
+    &:focus {
+      outline: none;
+    }
+  }
+}
+</style>
