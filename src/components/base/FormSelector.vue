b38db8784d7bdd835c2dfbf85bdf2ee9cc4c4634
Darya Volkova <anrich@bk.ru>
2022-03-31 09:25:23 +0300

    Changed selector component.

--- src/components/base/FormSelector.vue
+++ src/components/base/FormSelector.vue
@@ -1,14 +1,29 @@
 <template>
-<FormInput>
-<template #icon>^</template>  
-</FormInput>  
+  <FormInput>
+    <template #icon>
+      <span class="material-icons-round"> {{ icon }} </span>
+    </template>
+  </FormInput>
 </template>
 
 <script>
-import FormInput from "./FormInput.vue"
+import FormInput from "./FormInput.vue";
 export default {
   components: {
     FormInput,
-  }
-}
+  },
+
+  props: {
+    icon: {
+      type: String,
+    },
+  },
+};
 </script>
+
+<style lang="scss" scoped>
+span {
+  user-select: none;
+  cursor: pointer;
+}
+</style>
