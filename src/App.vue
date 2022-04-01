8f275f234e866eb755581af4b6528ef300aef345
Darya Volkova <anrich@bk.ru>
2022-04-01 13:27:06 +0300

    Deleted scroll. Added tabs.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <FormCard>
-    <div class="scroll">
-      <About></About>
-      <Address></Address>
-      <Passport></Passport>
-      <!-- <About></About> -->
+    <div class="view">
+      <div class="view__item" v-for="i in forms" :key="i">
+        {{ i }}
+      </div>
+    </div>
+    <div class="content">
+      <About v-if="currentForm == 0"></About>
+      <Address v-if="currentForm == 1"></Address>
+      <Passport v-if="currentForm == 2"></Passport>
+      <button @click="prevButton" v-if="this.currentForm >= 1">Prev</button>
+      <button @click="nextButton" v-if="this.currentForm <= 1">Next</button>
     </div>
   </FormCard>
 </template>
@@ -21,6 +27,29 @@ export default {
     About,
     Address,
     Passport,
+  },
+
+  data() {
+    return {
+      forms: [1, 2, 3],
+      currentForm: 0,
+    };
+  },
+
+  methods: {
+    prevButton() {
+      if (this.currentForm <= 0) {
+        return;
+      }
+      this.currentForm--;
+    },
+
+    nextButton() {
+      if (this.currentForm >= 2) {
+        return;
+      }
+      this.currentForm++;
+    },
   },
 };
 </script>
@@ -39,22 +68,33 @@ body {
   margin: 0px 60px;
   overflow: hidden;
 }
+
+p {
+  margin: 0px;
+  margin-bottom: 8px;
+}
 </style>
 
 <style lang="scss" scoped>
 .card {
+  display: flex;
+  flex-direction: column;
   width: 100%;
   max-width: 400px;
   height: 100%;
   max-height: 600px;
   overflow: hidden;
 
-  .scroll {
-    height: 100%;
-    overflow-y: auto;
+  .view {
+    display: flex;
+    justify-content: space-around;
+    padding: 6px 0px;
+  }
+
+  .content {
+    flex-grow: 1;
     padding: 8px;
     box-sizing: border-box;
-    // scrollbar-gutter: stable;
   }
 }
 </style>
